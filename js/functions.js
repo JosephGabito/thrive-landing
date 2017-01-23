d47197f63cc36d34041c21d8a4618dca9e60e8b0
Jasper <jasper.jardin1994@gmail.com>
2017-01-23 19:45:04 +0800

    Finish slider, Demo links, Sticky Header and typed js.

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,43 @@
 $(function () {
 
+    var index = 0;
+    setTimeout(function(){
+
+        $( '.slider-text-type-animate' ).typed({
+            strings: [
+                'E-Learning',
+                'Community',
+                'Company Portal',
+                'Small Business'
+            ],
+            typeSpeed: 70,
+            loop: true,
+            backDelay: 3000,
+            onStringTyped: function(){
+                __animate_slider()
+            }
+        });
+    }, 3000);
+
+    function __animate_slider(){
+        var max_index = parseInt( $( '.site-slider-image-container' ).length),
+            nodes = $( '.site-slider-image-container' );
+
+        index++,
+
+        nodes.removeClass( 'active' ),
+        $( nodes[index] ).addClass( 'active' ),
+
+        index === max_index - 1 && ( index = -1 )
+
+    }
+
+    // setInterval(__animate_slider, 3000);
+
     $( '#site-slider-outer-wrap' ).css({
+
         height: $( window ).height()
+        
     });
 
 });
