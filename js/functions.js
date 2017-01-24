5ab8e412050bc6ea06c51534fce001dc1e1fb072
Jasper <jasper.jardin1994@gmail.com>
2017-01-24 19:22:57 +0800

    Finished 80% of the Thrive landing page.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,8 @@
 $(function () {
 
     var index = 0;
+    var slider_images_array = [ "images/slider-1.jpg", "images/slider-2.jpg", "images/slider-3.jpg", "images/slider-4.jpg" ];
+
     setTimeout(function(){
 
         $( '.slider-text-type-animate' ).typed({
@@ -14,10 +16,27 @@ $(function () {
             loop: true,
             backDelay: 3000,
             onStringTyped: function(){
-                __animate_slider()
+                __animate_slider(),
+                __animate_slider_overlay()
             }
         });
     }, 3000);
+
+    $.each(slider_images_array, function( index, img_url ){
+        $.ajax({
+             url: img_url
+        });
+    });
+
+    $( '.site-slider-image-container' ).each(function(index, el){
+        
+        el = $(this);
+
+        var slider_image = el.find( '.site-slider-image' ).attr( "src" );
+
+        el.css( 'background-image', 'url(' + slider_image + ')' );
+
+    });
 
     function __animate_slider(){
         var max_index = parseInt( $( '.site-slider-image-container' ).length),
@@ -31,13 +50,28 @@ $(function () {
         index === max_index - 1 && ( index = -1 )
 
     }
+    function __animate_slider_overlay(){
 
-    // setInterval(__animate_slider, 3000);
+        var slider_overlay = $( '.site-section-content-overlay' );
+
+        if ( slider_overlay.hasClass('active') ) {
+
+            slider_overlay.removeClass('active');
+
+        } else {
+
+            slider_overlay.addClass('active');
+
+        }
+
+    }
+
+
 
     $( '#site-slider-outer-wrap' ).css({
 
         height: $( window ).height()
-        
+
     });
 
 });
