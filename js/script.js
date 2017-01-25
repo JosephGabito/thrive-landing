97f16ea61cb279015ff84c2adffa9abcfd09b07f
Jasper <jasper.jardin1994@gmail.com>
2017-01-25 18:20:20 +0800

    Finished the mark-up minor fixes are only needed.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 $( document ).ready( function( $ ) {
     "use strict";
 
+    var wow = new WOW({
+        animateClass: 'animated',
+        offset:       150,
+        callback:     function(box) {
+        }
+      });
+    wow.init();
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:3,
+        loop:true,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true,
+        autoWidth:true,
+        center:true,
+        dots:true,
+        dotsEach:true,
+        responsive:{
+                0:{
+                    items:1
+                },
+                480:{
+                    items:1
+                },
+                768:{
+                    items:2
+                }
+            }
+    });
+
+
     $( window ).scroll( function() {
 
         if ( $( this ).scrollTop() > 1 ) {
@@ -17,6 +51,16 @@ $( document ).ready( function( $ ) {
     });
 
     $(function() {
+        $('.site-nav-lists li a').on('click', function(e) {
+            e.preventDefault();
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+                $(this).parent().next('a').removeClass('active');
+            } else {
+                $(this).addClass('active');
+                $(this).parent().next('a').addClass('active');
+            }
+        });
 
         $( 'a[href*="#"]:not([href="#"])' ).click(function() {
 
@@ -30,13 +74,14 @@ $( document ).ready( function( $ ) {
 
                     $('html,body').animate({
 
-                        scrollTop: target.offset().top
+                        scrollTop: target.offset().top - $( '#header-container' ).height()
 
                     }, 500);
 
                     target.addClass( 'active' ).delay(500);
 
                     return false;
+
                 }
 
             }
